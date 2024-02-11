5835ad3ad6acc13b3582722bafee5b2317d15da1
Dmitriy Karpievich <todeath1987@gmail.com>
2024-02-11 15:07:26 +0300

    project init

---
+++ src/main/java/com/s4mda/app/S4mdaAppApplication.java
@@ -0,0 +1,13 @@
+package com.s4mda.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4mdaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S4mdaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s4mda/app/S4mdaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s4mda.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S4mdaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
